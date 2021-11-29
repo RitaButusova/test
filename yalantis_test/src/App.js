8dce00e34db606d164572f41307002bc8b82d376
Rita Butusova <butusovar@gmail.com>
2021-11-29 22:26:42 +0200

    link, hashRouter

--- yalantis_test/src/App.js
+++ yalantis_test/src/App.js
@@ -1,10 +1,23 @@
 import "./App.css";
-import { useRoutes } from 'hookrouter';
-import routes from "./routes";
+import { HashRouter, Switch, Route } from "react-router-dom";
+import Employees from "./components/Employees";
+//import { useRoutes } from 'hookrouter';
+//import routes from "./routes";
 
-function App() {
+/*function App() {
   const routeResult = useRoutes(routes);
   return routeResult;
 }
 
 export default App;
+*/
+
+export default function App() {
+  return (
+    <HashRouter basename="/">
+      <Switch>
+        <Route exact path="/employees" component={Employees}></Route>
+      </Switch>
+    </HashRouter>
+  );
+}
